3ce66084a8ed6de18fff7ef37cecb502af1c6101
Chris Eastwood <eastwood.chris@gmail.com>
2014-04-07 21:05:57 -0700

    Updated the checkstyle output to be formatted properly

--- CheckDebugTask.php
+++ CheckDebugTask.php
@@ -46,10 +46,10 @@ class CheckDebugTask extends Task {
     // Prepare some variables
     $pattern = $this->pattern;
     $line_number = 1;
+    $errors = array();
 
     // Open the input and output files
     $handle = fopen($this->inputFile, 'r');
-    $writer = new FileWriter($this->outputFile, $append = TRUE);
 
     // If we have a pattern and file we can start reading
     if ($handle && $pattern) {
@@ -60,23 +60,78 @@ class CheckDebugTask extends Task {
           // Check if it's commented
           if (preg_match("/\/\/.*$pattern|\/\*.*$pattern.*\*\/|\*.*$pattern/", $line)) {
             $this->log("Commented debug code detected: $line [line:$line_number]");
-            $writer->write("<error line=\"$line_number\" column=\"1\" severity=\"warning\" message=\"Commented debug code detected: $line\" source=\"CheckDebugTask\"/>\n");
+            $errors[] = array(
+              'line' => $line_number,
+              'column' => 1,
+              'severity' => 'warning',
+              'message' => "Commented debug code detected: $line",
+              'source' => 'CheckDebugTask',
+            );
           }
           else {
             $this->log("Uncommented debug code detected: $line [line:$line_number]");
-            $writer->write("<error line=\"$line_number\" column=\"1\" severity=\"error\" message=\"Uncommented debug code detected: $line\" source=\"CheckDebugTask\"/>\n");
+            $errors[] = array(
+              'line' => $line_number,
+              'column' => 1,
+              'severity' => 'error',
+              'message' => "Uncommented debug code detected: $line",
+              'source' => 'CheckDebugTask',
+            );
           }
         }
         $line_number++;
       }
     }
     else {
-      throw new BuildException("Unable to read file: {$this->filepath}");
+      throw new BuildException("Unable to read file: {$this->inputFile}");
     }
 
-    // Close the files
+    // If there are errors we need to add them to our checkstyle output xml
+    if ($errors) {
+      // First get the SimpleXML Document
+      $xml = $this->getSimpleXML();
+      // We'll add a new <file> element
+      $output = $xml->addChild('file');
+      $output->addAttribute('name', $this->inputFile);
+      // Loop through the errors and add the error element
+      foreach ($errors as $error) {
+        $item = $output->addChild('error');
+        foreach ($error as $attribute => $value) {
+          $item->addAttribute($attribute, $value);
+        }
+      }
+      $this->outputXML($xml);
+    }
+
+    // Close the file
     fclose($handle);
-    $writer->close();
+  }
+
+  /**
+   * Prepares the XML output
+   * @return [type] [description]
+   */
+  private function getSimpleXML() {
+    // First check if the outputFile exists
+    if (file_exists($this->outputFile)) {
+      $xml = simplexml_load_file($this->outputFile);
+    }
+    else {
+      $xml = simplexml_load_string("<?xml version=\"1.0\" encoding=\"UTF-8\"?><checkstyle></checkstyle>");
+    }
+    return $xml;
+  }
+
+  /**
+   * Helper function to format the xml output and save the file
+   */
+  private function outputXML($simpleXML) {
+    $dom = new DOMDocument('1.0');
+    $dom->preserveWhiteSpace = false;
+    $dom->formatOutput = true;
+    $dom->loadXML($simpleXML->asXML());
+    $dom->saveXML();
+    $dom->save($this->outputFile);
   }
 }
 
